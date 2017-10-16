721d115d5d9ef20d842ccb1b75390a18702d47b1
pjunwei95 <junwei35@hotmail.com>
2017-10-16 23:22:01 +0800

    DeletedTagCommand: updated

--- src/main/java/seedu/address/logic/commands/DeleteTagCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteTagCommand.java
@@ -1,44 +1,135 @@
 package seedu.address.logic.commands;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
+import static java.util.Objects.requireNonNull;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.CollectionUtil;
 import seedu.address.logic.commands.exceptions.CommandException;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+import seedu.address.model.person.Person;
 import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.model.person.exceptions.DuplicatePersonException;
+import seedu.address.model.person.exceptions.PersonNotFoundException;
+import seedu.address.model.tag.Tag;
 
 /**
  * Deletes a person identified using it's last displayed index from the address book.
  */
 public class DeleteTagCommand extends UndoableCommand {
 
-    public static final String COMMAND_WORD = "delete";
+    public static final String COMMAND_WORD = "delete/t";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Deletes the tag identified by the index number used in the last person listing.\n"
-            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Parameters: INDEX (must be a positive integer)"
+            + "TAG (must match tag)\n"
             + "Example: " + COMMAND_WORD + " 1";
-
+    //delete/t 1 friends
     public static final String MESSAGE_DELETE_TAG_SUCCESS = "Deleted Tag: %1$s";
+    public static final String MESSAGE_NOT_DELETED = "At least one field to edit must be provided.";
+    public static final String MESSAGE_DUPLICATE_TAG = "This person already exists in the address book.";
 
-    private final Index targetIndex;
+    private final Index index;
+    private final EditTagDescriptor editTagDescriptor;
 
-    public DeleteTagCommand(Index targetIndex) {
-        this.targetIndex = targetIndex;
+    /**
+     * @param index of the person in the filtered person list to edit
+     * @param editTagDescriptor details to edit the person with
+     */
+    public DeleteTagCommand(Index index, EditTagDescriptor editTagDescriptor) {
+        requireNonNull(index);
+        requireNonNull(editTagDescriptor);
+
+        this.index = index;
+        this.editTagDescriptor = new EditTagDescriptor(editTagDescriptor);
     }
-
 
     @Override
     public CommandResult executeUndoableCommand() throws CommandException {
         List<ReadOnlyPerson> lastShownList = model.getFilteredPersonList();
 
-        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+        if (index.getZeroBased() >= lastShownList.size()) {
             throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
         }
 
-        ReadOnlyPerson tagToDelete = lastShownList.get(targetIndex.getZeroBased());
+        ReadOnlyPerson personToEdit = lastShownList.get(index.getZeroBased());
+        Person editedPerson = createTagEditedPerson(personToEdit, new EditTagDescriptor(editTagDescriptor));
 
-        return new CommandResult(String.format(MESSAGE_DELETE_TAG_SUCCESS, tagToDelete));
+        try {
+            model.updatePerson(personToEdit, editedPerson);
+        } catch (DuplicatePersonException dpe) {
+            throw new CommandException(MESSAGE_DUPLICATE_TAG);
+        } catch (PersonNotFoundException pnfe) {
+            throw new AssertionError("The target person cannot be missing");
+        }
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+
+        return new CommandResult(String.format(MESSAGE_DELETE_TAG_SUCCESS, editedPerson));
     }
 
+    /**
+     * Creates and returns a {@code Person} with the details of {@code personToEdit}
+     * edited with {@code editPersonDescriptor}.
+     */
+    private static Person createTagEditedPerson(ReadOnlyPerson personToEdit,
+                                             EditTagDescriptor editTagDescriptor) {
+        assert personToEdit != null;
+
+        Set<Tag> updatedTags = editTagDescriptor.getTags().orElse(personToEdit.getTags());
+
+        return new Person(personToEdit.getName(), personToEdit.getPhone(), personToEdit.getEmail(),
+                personToEdit.getAddress(), personToEdit.getDateOfBirth(), personToEdit.getRemark(), updatedTags);
+    }
+
+    /**
+     * Stores the details to edit the person with. Each non-empty field value will replace the
+     * corresponding field value of the person.
+     */
+    public static class EditTagDescriptor {
+        private Set<Tag> tags;
+
+        public EditTagDescriptor() {
+        }
+
+        public EditTagDescriptor(EditTagDescriptor toCopy) {
+            this.tags = toCopy.tags;
+        }
+
+        /**
+         * Returns true if at least one field is edited.
+         */
+        public boolean isTagEdited() {
+            return CollectionUtil.isAnyNonNull(this.tags);
+        }
+
+        public void setTags(Set<Tag> tags) {
+            this.tags = tags;
+        }
+
+        public Optional<Set<Tag>> getTags() {
+            return Optional.ofNullable(tags);
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            // short circuit if same object
+            if (other == this) {
+                return true;
+            }
+
+            // instanceof handles nulls
+            if (!(other instanceof EditTagDescriptor)) {
+                return false;
+            }
+
+            // state check
+            EditTagDescriptor e = (EditTagDescriptor) other;
+
+            return getTags().equals(e.getTags());
+        }
+    }
 }
