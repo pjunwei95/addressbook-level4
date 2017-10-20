b66d0149d490ac48dcafece2d9a20b6886c0ae60
Ronak Lakhotia <ronak.lakhotia999@gmail.com>
2017-10-20 23:07:24 +0800

    Check for incorrect Path files

--- src/main/java/seedu/address/logic/parser/PhotoCommandParser.java
+++ src/main/java/seedu/address/logic/parser/PhotoCommandParser.java
@@ -1,8 +1,11 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_IMAGE;
 
 import java.util.stream.Stream;
+
+import java.io.File;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
@@ -27,6 +30,14 @@ public class PhotoCommandParser implements Parser<PhotoCommand> {
         if (trimmedArgs.isEmpty() || Keywords.length != 2) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, PhotoCommand.MESSAGE_USAGE));
+        }
+        String home = System.getProperty("user.home");
+        String inputFile = Keywords[0];
+        File workingDirectory = new File(home);
+        File testFile = new File(workingDirectory, inputFile);
+        if(!testFile.exists()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_IMAGE, PhotoCommand.MESSAGE_USAGE));
         }
 
         try {
