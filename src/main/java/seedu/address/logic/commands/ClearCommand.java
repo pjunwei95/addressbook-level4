e937545026f42ddb63b96d2d33efc7eff5ed320a
pjunwei95 <junwei35@hotmail.com>
2017-10-24 02:48:40 +0800

    ClearCommand: restored

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -3,7 +3,6 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 
 import seedu.address.model.AddressBook;
-import seedu.address.ui.ClearConfirmation;
 
 /**
  * Clears the address book.
@@ -12,19 +11,12 @@ public class ClearCommand extends UndoableCommand {
 
     public static final String COMMAND_WORD = "clear";
     public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
-    public static final String MESSAGE_FAILURE = "Address book has not been cleared!";
 
 
     @Override
     public CommandResult executeUndoableCommand() {
         requireNonNull(model);
-        ClearConfirmation confirmDialog = new ClearConfirmation();
-        if (confirmDialog.isClearCommand()) {
-            model.resetData(new AddressBook());
-            return new CommandResult(MESSAGE_SUCCESS);
-        }
-        else {
-            return new CommandResult(MESSAGE_FAILURE);
-        }
+        model.resetData(new AddressBook());
+        return new CommandResult(MESSAGE_SUCCESS);
     }
 }
