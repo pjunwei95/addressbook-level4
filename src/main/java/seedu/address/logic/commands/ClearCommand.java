fdc1d0f4df44c6b5cdebc1bc8e661b2975a3ccee
pjunwei95 <junwei35@hotmail.com>
2017-10-23 20:36:55 +0800

    ClearConfirmation: Working template

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -1,8 +1,13 @@
 package seedu.address.logic.commands;
 
+import java.util.Optional;
+
 import static java.util.Objects.requireNonNull;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import seedu.address.model.AddressBook;
+import seedu.address.ui.ClearConfirmation;
 
 /**
  * Clears the address book.
@@ -11,12 +16,18 @@ public class ClearCommand extends UndoableCommand {
 
     public static final String COMMAND_WORD = "clear";
     public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
+    public static final String MESSAGE_FAILURE = "Address book has not been cleared!";
 
 
     @Override
     public CommandResult executeUndoableCommand() {
         requireNonNull(model);
-        model.resetData(new AddressBook());
-        return new CommandResult(MESSAGE_SUCCESS);
+        ClearConfirmation confirmDialog = new ClearConfirmation();
+        if (confirmDialog.isClearCommand()) {
+            model.resetData(new AddressBook());
+            return new CommandResult(MESSAGE_SUCCESS);
+        }
+        else
+            return new CommandResult(MESSAGE_FAILURE);
     }
 }
--- src/main/java/seedu/address/ui/ClearConfirmation.java
+++ src/main/java/seedu/address/ui/ClearConfirmation.java
@@ -1,22 +1,57 @@
 package seedu.address.ui;
 
 import java.util.Optional;
+import java.util.logging.Logger;
+
+import javax.swing.plaf.synth.Region;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.DialogPane;
+import javafx.stage.Stage;
+import seedu.address.commons.core.LogsCenter;
 
-public class ClearConfirmation {
-    @FXML
-    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-    alert.setTitle("Confirmation Dialog");
-    alert.setHeaderText("Look, a Confirmation Dialog");
-    alert.setContentText("Are you ok with this?");
+public class ClearConfirmation{
+/*
+    public static final String DEFAULT_PAGE = "default.html";
+    private static final String ICON = "/images/address_book_32.png";
+    private static final int MIN_HEIGHT = 600;
+    private static final int MIN_WIDTH = 450;
 
-    Optional<ButtonType> result = alert.showAndWait();
-    if (result.get() == ButtonType.OK){
-        // ... user chose OK
-    } else {
-        // ... user chose CANCEL or closed the dialog
+
+    private MainWindow mainWindow;
+    */
+
+    private static final String FXML = "ClearConfirmation.fxml";
+    private final Logger logger = LogsCenter.getLogger(this.getClass());
+
+    public ClearConfirmation(){
+        //super(FXML);
+        // Set dependencies
+
+        // Configure the UI
+        //registerAsAnEventHandler(this);
     }
+    public boolean isClearCommand() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        
+
+        alert.setHeaderText(null);
+        alert.setTitle("Confirmation Dialog");
+        alert.setContentText("Are you sure you want to clear the entire Address Book?");
+
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK){
+            // ... user chose OK
+            return true;
+        } else {
+            // ... user chose CANCEL or closed the dialog
+            return false;
+        }
+    }
+
+
+
 }
