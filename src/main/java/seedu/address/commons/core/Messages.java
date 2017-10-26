0bc5a984b71cf30c21a65d02d1fefb4de3f2154a
Ronak Lakhotia <ronak.lakhotia999@gmail.com>
2017-10-27 05:00:36 +0800

    Add a new package 'reminder' to Model package

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -12,7 +12,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_IMAGE = "Enter correct path, the path you entered may not be correct!\n"
             + "You may have entered the incorrect name of the file!";
     public static final String MESSAGE_NO_IMAGE_TO_DELETE = "The person has no Photo to be deleted.";
-    public static final String MESSAGE_FACEBOOK_SHOWN_SUCCESS = "FaceBook profile of the user displayed!";
     public static final String MESSAGE_NO_USERNAME = "This Person has no Facebook username!\n";
+    public static final String MESSAGE_INVALID_REMINDER_INDEX = "The person index provided is invalid";
 
 }
