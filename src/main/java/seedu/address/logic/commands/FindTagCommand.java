62480e158443cb8ed697a685c5842b1c3360f4ab
pjunwei95 <junwei35@hotmail.com>
2017-10-31 03:34:00 +0800

    FindTagCommand: modified command word and updated UG

--- src/main/java/seedu/address/logic/commands/FindTagCommand.java
+++ src/main/java/seedu/address/logic/commands/FindTagCommand.java
@@ -8,7 +8,7 @@ import seedu.address.model.tag.TagContainsKeywordsPredicate;
  */
 public class FindTagCommand extends Command {
 
-    public static final String COMMAND_WORD = "find/t";
+    public static final String COMMAND_WORD = "findtag";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose tag(s) contain any of "
             + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
