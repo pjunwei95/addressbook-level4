0b6e8f5b35b25748f481252f9850c62dddd463bf
Ronak Lakhotia <ronak.lakhotia999@gmail.com>
2017-11-10 02:39:41 +0800

    Check Again

--- src/test/java/seedu/address/logic/commands/PhotoCommandTest.java
+++ src/test/java/seedu/address/logic/commands/PhotoCommandTest.java
@@ -3,26 +3,19 @@ package seedu.address.logic.commands;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
-import static seedu.address.testutil.TypicalPersons.RANDOMNEW;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
-
-import java.util.List;
 
 import org.junit.Test;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
-import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.UndoRedoStack;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.person.ReadOnlyPerson;
-
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for {@code PhotoCommand}.
@@ -30,7 +23,10 @@ import seedu.address.model.person.ReadOnlyPerson;
 public class PhotoCommandTest {
 
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+
     /**
+     *
      * Out Of Bounds Exception Testing
      */
     @Test
@@ -42,30 +38,12 @@ public class PhotoCommandTest {
 
         assertCommandFailure(photoCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
     }
-    @Test
-    public void deleteFaliure() {
-        Index index = Index.fromOneBased(model.getFilteredPersonList().size() - 1);
-        String filePath = "delete";
-        PhotoCommand photoCommand = prepareCommand(index, filePath);
-        assertCommandFailure(photoCommand, model, Messages.MESSAGE_NO_IMAGE_TO_DELETE);
-    }
-    @Test
-    public void deleteSuccess() throws IllegalValueException {
-        Index index = Index.fromOneBased(model.getFilteredPersonList().size());
-
-        List<ReadOnlyPerson> lastShownList = model.getFilteredPersonList();
-        String filePath = "delete";
-        ReadOnlyPerson personToAddPhoto = lastShownList.get(index.getZeroBased());
-        PhotoCommand photoCommand = prepareCommand(index, filePath);
-        ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
-        assertCommandSuccess(photoCommand, model, String.format(
-                PhotoCommand.DELETE_SUCCESS, RANDOMNEW), expectedModel);
-    }
 
     /**
      * Returns a {@code PhotoCommand} with the parameter {@code index and Filepath}.
      */
     private PhotoCommand prepareCommand(Index index, String FilePath) {
+        //DeleteCommand deleteCommand = new DeleteCommand(index);
         PhotoCommand photoCommand = new PhotoCommand(index, FilePath);
         photoCommand.setData(model, new CommandHistory(), new UndoRedoStack());
         return photoCommand;
