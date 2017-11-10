b297632ef3704940124dc7ef6dd19fc034e09e5e
yangminxingnus <e0134082@u.nus.edu>
2017-11-10 18:10:52 +0800

    Add one testcase for remark

---
+++ src/test/java/seedu/address/model/person/RemarkTest.java
@@ -0,0 +1,21 @@
+package seedu.address.model.person;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class RemarkTest {
+    @Test
+    public void isValidRemark() {
+        // invalid remark
+        assertFalse(Remark.isValidRemark(" ")); // spaces only
+        assertFalse(Remark.isValidRemark("/LEC/1")); // missing module title
+        assertFalse(Remark.isValidRemark("CS2102//1")); // missing module type
+        assertFalse(Remark.isValidRemark("CS2102/LEC/")); // missing module number
+
+        // valid remark
+        assertTrue(Remark.isValidRemark("")); // empty string
+        assertTrue(Remark.isValidRemark("CS2102/LEC/1"));
+    }
+}
