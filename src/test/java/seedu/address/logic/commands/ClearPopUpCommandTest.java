ae5c89a23f4ca10379e58fe69fd30d684ed49ab4
Ronak Lakhotia <ronak.lakhotia999@gmail.com>
2017-11-11 21:54:55 +0800

    Update Tests

---
+++ src/test/java/seedu/address/logic/commands/ClearPopUpCommandTest.java
@@ -0,0 +1,39 @@
+package seedu.address.logic.commands;
+
+import static org.junit.Assert.assertEquals;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+public class ClearPopUpCommandTest {
+
+    @Test
+    public void execute_emptyAddressBook_success() {
+        Model model = new ModelManager();
+        assertCommandSuccessClear(model, ClearPopupCommand.MESSAGE_SUCCESS, model);
+    }
+
+    @Test
+    public void execute_nonEmptyAddressBook_success() {
+        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        assertCommandSuccessClear(model, ClearPopupCommand.MESSAGE_SUCCESS, model);
+    }
+
+    /**
+     * Executes the given {@code command}, confirms that <br>
+     * - the result message matches {@code expectedMessage} <br>
+     * - the {@code actualModel} matches {@code expectedModel}
+     */
+
+    public static void assertCommandSuccessClear( Model actualModel, String expectedMessage,
+                                            Model expectedModel) {
+
+        assertEquals(expectedMessage, "Weaver has been cleared!");;
+        assertEquals(expectedModel, actualModel);
+
+    }
+}
